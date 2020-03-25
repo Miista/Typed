f5b220651acd074c3472448bb75e8283a2e61d40
Søren Guldmund <sgu@immeo.dk>
2020-03-25 07:10:39 +0100

    Implement merge functionality from any two types to a third type

---
+++ TypeMerger.Tests/TypeMergerTests.cs
@@ -0,0 +1,413 @@
+using System;
+using System.Collections.Generic;
+using AutoFixture;
+using FluentAssertions;
+using Xunit;
+
+namespace TypeMerger.Tests
+{
+    internal class SourceWithConstructor {
+        public string Id { get; }
+        public string Name { get; }
+        public int? Age { get; }
+
+        public SourceWithConstructor(string id, string name, int? age) => (Id, Name, Age) = (id, name, age);
+    }
+
+    internal class DestinationWithConstructor {
+        public string Id { get; }
+        public string Name { get; }
+        public int? Age { get; }
+        
+        public DestinationWithConstructor(string id, string name, int? age) => (Id, Name, Age) = (id, name, age);
+    }
+    
+    internal class SourceWithSetters {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int? Age { get; set; }
+    }
+
+    internal class DestinationWithSetters {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int? Age { get; set; }
+    }
+
+    internal class SourceWithPropertiesAndMissingProperties
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+    
+    internal class DestinationWithPropertiesAndMissingProperties
+    {
+        public string Id { get; set; }
+    }
+    
+    internal class SourceWithConstructorAndMissingProperties
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public int? Age { get; }
+        public DateTime Birthdate { get; }
+
+        public SourceWithConstructorAndMissingProperties(string id, string name, int? age, DateTime birthdate)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Birthdate = birthdate;
+        }
+    }
+    
+    internal class DestinationWithConstructorAndMissingProperties
+    {
+        public string Id { get; }
+
+        public DestinationWithConstructorAndMissingProperties(string id)
+        {
+            Id = id;
+        }
+    }
+    
+    public class TypeMergerTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Can_merge_types_using_constructors(
+            string sourceId, string sourceName, int? sourceAge,
+            string destinationId, string destinationName, int? destinationAge,
+            string expectedId, string expectedName, int? expectedAge)
+        {
+            // Arrange
+            var source = new SourceWithConstructor(sourceId, sourceName, sourceAge);
+            var destination = new DestinationWithConstructor(destinationId, destinationName, destinationAge);
+
+            // Act
+            var result = TypeMerger.Merge<SourceWithConstructor, SourceWithConstructor, DestinationWithConstructor>(source, destination);
+
+            // Assert
+            result.Age.Should().Be(expectedAge);
+            result.Id.Should().Be(expectedId);
+            result.Name.Should().Be(expectedName);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Can_merge_types_using_properties(
+            string sourceId, string sourceName, int? sourceAge,
+            string destinationId, string destinationName, int? destinationAge,
+            string expectedId, string expectedName, int? expectedAge)
+        {
+            // Arrange
+            var source = new SourceWithSetters
+            {
+                Id = sourceId,
+                Age = sourceAge,
+                Name = sourceName
+            };
+            var destination = new DestinationWithSetters
+            {
+                Id = destinationId,
+                Age = destinationAge,
+                Name = destinationName
+            };
+
+            // Act
+            var result = TypeMerger.Merge<SourceWithSetters, SourceWithSetters, DestinationWithSetters>(source, destination);
+
+            // Assert
+            result.Age.Should().Be(expectedAge);
+            result.Id.Should().Be(expectedId);
+            result.Name.Should().Be(expectedName);
+        }
+        
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Can_merge_from_property_based_to_constructor_based(
+            string sourceId, string sourceName, int? sourceAge,
+            string destinationId, string destinationName, int? destinationAge,
+            string expectedId, string expectedName, int? expectedAge)
+        {
+            // Arrange
+            var source = new SourceWithConstructor(sourceId, sourceName, sourceAge);
+            var destination = new DestinationWithSetters
+            {
+                Id = destinationId,
+                Age = destinationAge,
+                Name = destinationName
+            };
+
+            // Act
+            var result = TypeMerger.Merge<SourceWithConstructor, SourceWithConstructor, DestinationWithSetters>(source, destination);
+
+            // Assert
+            result.Age.Should().Be(expectedAge);
+            result.Id.Should().Be(expectedId);
+            result.Name.Should().Be(expectedName);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Can_merge_from_property_based_to_property_based(
+            string sourceId, string sourceName, int? sourceAge,
+            string destinationId, string destinationName, int? destinationAge,
+            string expectedId, string expectedName, int? expectedAge)
+        {
+            // Arrange
+            var source = new SourceWithConstructor(sourceId, sourceName, sourceAge);
+            var destination = new SourceWithConstructor(destinationId, destinationName, destinationAge);
+
+            // Act
+            var result = TypeMerger.Merge<SourceWithConstructor, SourceWithConstructor, SourceWithConstructor>(source, destination);
+
+            // Assert
+            result.Age.Should().Be(expectedAge);
+            result.Id.Should().Be(expectedId);
+            result.Name.Should().Be(expectedName);
+        }
+        
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Can_merge_from_constructor_based_to_property_based(
+            string sourceId, string sourceName, int? sourceAge,
+            string destinationId, string destinationName, int? destinationAge,
+            string expectedId, string expectedName, int? expectedAge)
+        {
+            // Arrange
+            var source = new SourceWithSetters
+            {
+                Id = sourceId,
+                Age = sourceAge,
+                Name = sourceName
+            };
+            var destination = new DestinationWithConstructor(destinationId, destinationName, destinationAge);
+
+            // Act
+            var result = TypeMerger.Merge<SourceWithSetters, SourceWithSetters, DestinationWithConstructor>(source, destination);
+
+            // Assert
+            result.Age.Should().Be(expectedAge);
+            result.Id.Should().Be(expectedId);
+            result.Name.Should().Be(expectedName);
+        }
+        
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Can_merge_from_constructor_based_to_constructor_based(
+            string sourceId, string sourceName, int? sourceAge,
+            string destinationId, string destinationName, int? destinationAge,
+            string expectedId, string expectedName, int? expectedAge)
+        {
+            // Arrange
+            var source = new SourceWithConstructor(sourceId, sourceName, sourceAge);
+            var destination = new DestinationWithConstructor(destinationId, destinationName, destinationAge);
+
+            // Act
+            var result = TypeMerger.Merge<SourceWithConstructor, SourceWithConstructor, DestinationWithConstructor>(source, destination);
+
+            // Assert
+            result.Age.Should().Be(expectedAge);
+            result.Id.Should().Be(expectedId);
+            result.Name.Should().Be(expectedName);
+        }
+        
+        [Fact]
+        public void Can_merge_using_constructor_when_right_side_is_missing_properties()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var left = fixture.Create<SourceWithPropertiesAndMissingProperties>();
+            var right = fixture.Create<DestinationWithPropertiesAndMissingProperties>();
+            
+            // Act
+            var result = TypeMerger.Merge<SourceWithPropertiesAndMissingProperties, SourceWithPropertiesAndMissingProperties, DestinationWithPropertiesAndMissingProperties>(left, right);
+            
+            // Assert
+            result.Id.Should().Be(right.Id);
+            result.Age.Should().Be(left.Age);
+            result.Name.Should().Be(left.Name);
+            result.Birthdate.Should().Be(left.Birthdate);
+        }
+        
+        [Fact]
+        public void Can_merge_using_properties_when_right_side_is_missing_properties()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var left = fixture.Create<SourceWithConstructorAndMissingProperties>();
+            var right = fixture.Create<DestinationWithConstructorAndMissingProperties>();
+            
+            // Act
+            var result = TypeMerger.Merge<SourceWithConstructorAndMissingProperties, SourceWithConstructorAndMissingProperties, DestinationWithConstructorAndMissingProperties>(left, right);
+            
+            // Assert
+            result.Id.Should().Be(right.Id);
+            result.Age.Should().Be(left.Age);
+            result.Name.Should().Be(left.Name);
+            result.Birthdate.Should().Be(left.Birthdate);
+        }
+
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                yield return new object[]{null, null, null, null, null, null, null, null, null};
+                yield return new object[]{null, null, 1,    null, null, null, null, null, 1};
+                yield return new object[]{null, "1",  null, null, null, null, null, "1",  null};
+                yield return new object[]{null, "1",  1,    null, null, null, null, "1",  1};
+                yield return new object[]{"1",  null, null, null, null, null, "1",  null, null};
+                yield return new object[]{"1",  null, 1,    null, null, null, "1",  null, 1};
+                yield return new object[]{"1",  "1",  null, null, null, null, "1",  "1",  null};
+                yield return new object[]{"1",  "1",  1,    null, null, null, "1",  "1",  1};
+                yield return new object[]{null, null, null, null, null, 2,    null, null, 2};
+                yield return new object[]{null, null, null, null, "2",  null, null, "2",  null};
+                yield return new object[]{null, null, null, null, "2",  2,    null, "2",  2};
+                yield return new object[]{null, null, null, "2",  null, null, "2",  null, null};
+                yield return new object[]{null, null, null, "2",  null, 2,    "2",  null, 2};
+                yield return new object[]{null, null, null, "2",  "2",  null, "2",  "2",  null};
+                yield return new object[]{null, null, null, "2",  "2",  2,    "2",  "2",  2};
+                yield return new object[]{null, null, 1,    null, null, 2,    null, null, 2};
+                yield return new object[]{null, "1",  null, null, "2",  null, null, "2",  null};
+                yield return new object[]{null, "1",  1,    null, "2",  2,    null, "2",  2};
+                yield return new object[]{"1",  null, null, "2",  null, null, "2",  null, null};
+                yield return new object[]{"1",  null, 1,    "2",  null, 2,    "2",  null, 2};
+                yield return new object[]{"1",  "1",  1,    "2",  "2",  2,    "2",  "2",  2};
+                yield return new object[]{"1",  "1",  1,    null, null, null, "1",  "1",  1};
+                yield return new object[]{"1",  "1",  1,    null, null, 2,    "1",  "1",  2};
+                yield return new object[]{"1",  "1",  1,    null, "2",  null, "1",  "2",  1};
+                yield return new object[]{"1",  "1",  1,    null, "2",  2,    "1",  "2",  2};
+                yield return new object[]{"1",  "1",  1,    "2",  null, null, "2",  "1",  1};
+                yield return new object[]{"1",  "1",  1,    "2",  null, 2,    "2",  "1",  2};
+                yield return new object[]{"1",  "1",  1,    "2",  "2",  null, "2",  "2",  1};
+                yield return new object[]{"1",  "1",  1,    "2",  "2",  2,    "2",  "2",  2};
+                yield return new object[]{null, null, null, "2",  "2",  2,    "2",  "2",  2};
+                yield return new object[]{null, null, 1,    "2",  "2",  2,    "2",  "2",  2};
+                yield return new object[]{null, "1", null,  "2",  "2",  2,    "2",  "2",  2};
+                yield return new object[]{null, "1", 1,     "2",  "2",  2,    "2",  "2",  2};
+                yield return new object[]{"1",  null, null, "2",  "2",  2,    "2",  "2",  2};
+                yield return new object[]{"1",  null, 1,    "2",  "2",  2,    "2",  "2",  2};
+                yield return new object[]{"1",  "1",  null, "2",  "2",  2,    "2",  "2",  2};
+            }
+        }
+
+        [Theory]
+        [InlineData(null, "RightId", "RightId")]
+        [InlineData("LeftId", null, null)]
+        [InlineData("LeftId", "RightId", "RightId")]
+        public void Takes_value_from_right_side_if_left_side_is_unmatched_using_constructor(string leftId, string rightId, string targetId)
+        {
+            // Arrange
+            var left = new AnotherSideWithConstructor(leftId);
+            var right = new OneSideWithConstructor(rightId);
+
+            // Act
+            var result = TypeMerger.Merge<TargetOfLeftAndRightWithConstructor, AnotherSideWithConstructor, OneSideWithConstructor>(left, right);
+
+            // Assert
+            result.OneTargetId.Should().Be(targetId);
+        }
+        
+        [Theory]
+        [InlineData(null, "RightId", "RightId")]
+        [InlineData("LeftId", null, null)]
+        [InlineData("LeftId", "RightId", "RightId")]
+        public void Takes_value_from_right_side_if_left_side_is_unmatched_using_properties(string leftId, string rightId, string targetId)
+        {
+            // Arrange
+            var left = new AnotherSideWithProperties {AnotherTargetId = leftId};
+            var right = new OneSideWithProperties {OneTargetId = rightId};
+
+            // Act
+            var result = TypeMerger.Merge<TargetOfLeftAndRightWithProperties, AnotherSideWithProperties, OneSideWithProperties>(left, right);
+
+            // Assert
+            result.OneTargetId.Should().Be(targetId);
+        }
+        
+        [Theory]
+        [InlineData("LeftId", null, "LeftId")]
+        [InlineData(null, "RightId", null)]
+        [InlineData("LeftId", "RightId", "LeftId")]
+        public void Takes_value_from_left_side_if_right_side_is_unmatched_using_constructor(string leftId, string rightId, string targetId)
+        {
+            // Arrange
+            var left = new AnotherSideWithConstructor(leftId);
+            var right = new OneSideWithConstructor(rightId);
+
+            // Act
+            var result = TypeMerger.Merge<TargetOfLeftAndRightWithConstructor, AnotherSideWithConstructor, OneSideWithConstructor>(left, right);
+
+            // Assert
+            result.AnotherTargetId.Should().Be(targetId);
+        }
+        
+        [Theory]
+        [InlineData("LeftId", null, "LeftId")]
+        [InlineData(null, "RightId", null)]
+        [InlineData("LeftId", "RightId", "LeftId")]
+        public void Takes_value_from_left_side_if_right_side_is_unmatched_using_properties(string leftId, string rightId, string targetId)
+        {
+            // Arrange
+            var left = new AnotherSideWithProperties {AnotherTargetId = leftId};
+            var right = new OneSideWithProperties {OneTargetId = rightId};
+
+            // Act
+            var result = TypeMerger.Merge<TargetOfLeftAndRightWithProperties, AnotherSideWithProperties, OneSideWithProperties>(left, right);
+
+            // Assert
+            result.AnotherTargetId.Should().Be(targetId);
+        }
+    }
+    
+    internal class OneSideWithConstructor
+    {
+        public string OneTargetId { get; }
+
+        public OneSideWithConstructor(string oneTargetId)
+        {
+            OneTargetId = oneTargetId;
+        }
+    }
+
+    internal class AnotherSideWithConstructor
+    {
+        public string AnotherTargetId { get; }
+        
+        public AnotherSideWithConstructor(string anotherTargetId)
+        {
+            AnotherTargetId = anotherTargetId;
+        }
+    }
+
+    internal class TargetOfLeftAndRightWithConstructor
+    {
+        public string OneTargetId { get; }
+        public string AnotherTargetId { get; }
+
+        public TargetOfLeftAndRightWithConstructor(string oneTargetId, string anotherTargetId)
+        {
+            OneTargetId = oneTargetId;
+            AnotherTargetId = anotherTargetId;
+        }
+    }
+    
+    internal class OneSideWithProperties
+    {
+        public string OneTargetId { get; set; }
+    }
+
+    internal class AnotherSideWithProperties
+    {
+        public string AnotherTargetId { get; set; }
+    }
+
+    internal class TargetOfLeftAndRightWithProperties
+    {
+        public string OneTargetId { get; set; }
+        public string AnotherTargetId { get; set; }
+    }
+}
---
+++ TypeMerger/TypeMerger.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TypeMerger
+{
+    public class TypeMerger
+    {
+        public static TDestination Merge<TDestination, TLeft, TRight>(TLeft left, TRight right)
+            where TDestination : class
+        {
+            if (left == null) throw new ArgumentNullException(nameof(left));
+            if (right == null) throw new ArgumentNullException(nameof(right));
+
+            var constructorInfo = GetSuitableConstructor<TDestination>();
+
+            return constructorInfo == null
+                ? MergeByProperty<TDestination, TLeft, TRight>(left, right)
+                : MergeByConstructor<TDestination, TLeft, TRight>(left, right, constructorInfo);
+        }
+
+        private static ConstructorInfo GetSuitableConstructor<T>()
+        {
+            return typeof(T)
+                .GetConstructors()
+                .OrderByDescending(info => info.GetParameters().Length)
+                .FirstOrDefault(info => info.GetParameters().Length > 0);
+        }
+
+        private static TDestination MergeByConstructor<TDestination, TLeft, TRight>(
+            TLeft left,
+            TRight right,
+            ConstructorInfo constructorInfo)
+            where TDestination : class
+        {
+            var leftProperties = GetProperties<TLeft>();
+            var rightProperties = GetProperties<TRight>();
+
+            var parameters = new List<object>();
+
+            foreach (var parameter in constructorInfo.GetParameters())
+            {
+                leftProperties.TryGetValue(parameter.Name.ToLowerInvariant(), out var leftProperty);
+                var leftValue = leftProperty?.GetValue(left);
+                
+                rightProperties.TryGetValue(parameter.Name.ToLowerInvariant(), out var rightProperty);
+                var rightValue = rightProperty?.GetValue(right);
+
+                var value = rightValue ?? leftValue;
+                
+                parameters.Add(value);
+            }
+
+            var constructedInstance = constructorInfo.Invoke(parameters.ToArray()) as TDestination;
+
+            return constructedInstance;
+        }
+
+        private static TDestination MergeByProperty<TDestination, TLeft, TRight>(
+            TLeft left, 
+            TRight right)
+            where TDestination : class
+        {
+            var leftProperties = GetProperties<TLeft>();
+            var rightProperties = GetProperties<TRight>();
+            var destinationProperties = GetProperties<TDestination>();
+
+            var constructedInstance = Activator.CreateInstance<TDestination>();
+            
+            foreach (var parameter in destinationProperties)
+            {
+                leftProperties.TryGetValue(parameter.Key.ToLowerInvariant(), out var leftProperty);
+                var leftValue = leftProperty?.GetValue(left);
+                
+                rightProperties.TryGetValue(parameter.Key.ToLowerInvariant(), out var rightProperty);
+                var rightValue = rightProperty?.GetValue(right);
+
+                var value = rightValue ?? leftValue;
+                
+                var setMethod = parameter.Value.GetSetMethod();
+                setMethod.Invoke(constructedInstance, new[] {value});
+            }
+
+            return constructedInstance;
+        }
+
+        private static Dictionary<string, PropertyInfo> GetProperties<T>() => typeof(T).GetProperties().ToDictionary(info => info.Name.ToLowerInvariant());
+    }
+}
