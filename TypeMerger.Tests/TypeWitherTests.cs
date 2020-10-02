ed1af37e6246735fb8b02ef882c934af35d8e9c7
Søren Guldmund <sgu@immeo.dk>
2020-10-02 09:49:31 +0200

    Add With extension method

--- TypeMerger.Tests/TypeWitherTests.cs
+++ TypeMerger.Tests/TypeWitherTests.cs
@@ -8,33 +8,6 @@ namespace TypeMerger.Tests
     {
         [Theory]
         [MemberData(nameof(TestData))]
-        public void Can_with_using_expressions_using_properties(
-            string sourceId, string sourceName, int? sourceAge,
-            string withId, string withName, int? withAge,
-            string expectedId, string expectedName, int? expectedAge)
-        {
-            // Arrange
-            var source = new SourceWithSetters
-            {
-                Id = sourceId,
-                Name = sourceName,
-                Age = sourceAge
-            };
-            
-            // Act
-            SourceWithSetters result = TypeWither.GetBuilder(source)
-                .With(_ => _.Id, withId)
-                .With(_ => _.Age, withAge)
-                .With(_ => _.Name, withName);
-
-            // Assert
-            result.Age.Should().Be(expectedAge);
-            result.Id.Should().Be(expectedId);
-            result.Name.Should().Be(expectedName);
-        }
-        
-        [Theory]
-        [MemberData(nameof(TestData))]
         public void Can_with_using_properties(
             string sourceId, string sourceName, int? sourceAge,
             string withId, string withName, int? withAge,
@@ -62,6 +35,33 @@ namespace TypeMerger.Tests
 
         [Theory]
         [MemberData(nameof(TestData))]
+        public void Can_with_using_expressions_using_properties(
+            string sourceId, string sourceName, int? sourceAge,
+            string withId, string withName, int? withAge,
+            string expectedId, string expectedName, int? expectedAge)
+        {
+            // Arrange
+            var source = new SourceWithSetters
+            {
+                Id = sourceId,
+                Name = sourceName,
+                Age = sourceAge
+            };
+            
+            // Act
+            SourceWithSetters result = TypeWither.GetBuilder(source)
+                .With(_ => _.Id, withId)
+                .With(_ => _.Age, withAge)
+                .With(_ => _.Name, withName);
+
+            // Assert
+            result.Age.Should().Be(expectedAge);
+            result.Id.Should().Be(expectedId);
+            result.Name.Should().Be(expectedName);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
         public void Can_with_using_constructor(
             string sourceId, string sourceName, int? sourceAge,
             string withId, string withName, int? withAge,
@@ -69,12 +69,6 @@ namespace TypeMerger.Tests
         {
             // Arrange
             var source = new SourceWithConstructor(sourceId, sourceName, sourceAge);
-            var withProperties = new List<(string propertyName, object propertyValue)>
-            {
-                (nameof(SourceWithConstructor.Id), withId),
-                (nameof(SourceWithConstructor.Name), withName),
-                (nameof(SourceWithConstructor.Age), withAge)
-            }.ToArray();
 
             // Act
             SourceWithConstructor result = TypeWither.GetBuilder(source)
@@ -109,7 +103,56 @@ namespace TypeMerger.Tests
             result.Id.Should().Be(expectedId);
             result.Name.Should().Be(expectedName);
         }
-        
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Can_with_using_constructor_by_extension_with_method(
+            string sourceId, string sourceName, int? sourceAge,
+            string withId, string withName, int? withAge,
+            string expectedId, string expectedName, int? expectedAge)
+        {
+            // Arrange
+            var source = new SourceWithConstructor(sourceId, sourceName, sourceAge);
+
+            // Act
+            SourceWithConstructor result = source
+                .With(_ => _.Id, withId)
+                .With(_ => _.Name, withName)
+                .With(_ => _.Age, withAge);
+
+            // Assert
+            result.Age.Should().Be(expectedAge);
+            result.Id.Should().Be(expectedId);
+            result.Name.Should().Be(expectedName);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Can_with_using_properties_by_extension_with_method(
+            string sourceId, string sourceName, int? sourceAge,
+            string withId, string withName, int? withAge,
+            string expectedId, string expectedName, int? expectedAge)
+        {
+            // Arrange
+            var source = new SourceWithSetters
+            {
+                Id = sourceId,
+                Name = sourceName,
+                Age = sourceAge
+            };
+            
+            // Act
+            SourceWithSetters result = source
+                .With(_ => _.Id, withId)
+                .With(_ => _.Name, withName)
+                .With(_ => _.Age, withAge);
+
+            // Assert
+            result.Age.Should().Be(expectedAge);
+            result.Id.Should().Be(expectedId);
+            result.Name.Should().Be(expectedName);
+        }
+
         public static IEnumerable<object[]> TestData
         {
             get
