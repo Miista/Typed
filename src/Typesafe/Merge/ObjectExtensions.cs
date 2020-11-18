841b87ab277999af3632710ca6ed0dfa79372fdf
Søren Guldmund <sgu@immeo.dk>
2020-11-18 14:33:13 +0100

    Make merge functionality pass tests

--- src/Typesafe/Merge/ObjectExtensions.cs
+++ src/Typesafe/Merge/ObjectExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Typesafe.Utils;
 
@@ -14,10 +15,14 @@ namespace Typesafe.Merge
             if (right == null) throw new ArgumentNullException(nameof(right));
 
             var constructorInfo = ReflectionUtils.GetSuitableConstructor<TDestination>();
+            if (constructorInfo != null)
+                return MergeByConstructor<TDestination, TLeft, TRight>(left, right, constructorInfo);
 
-            return constructorInfo == null
-                ? MergeByProperty<TDestination, TLeft, TRight>(left, right)
-                : MergeByConstructor<TDestination, TLeft, TRight>(left, right, constructorInfo);
+            var defaultConstructorInfo = ReflectionUtils.GetDefaultConstructor<TDestination>();
+            if (defaultConstructorInfo != null)
+                return MergeByConstructor<TDestination, TLeft, TRight>(left, right, defaultConstructorInfo);
+
+            return MergeByProperty<TDestination, TLeft, TRight>(left, right);
         }
 
         private static TDestination MergeByConstructor<TDestination, TLeft, TRight>(
@@ -26,40 +31,80 @@ namespace Typesafe.Merge
             ConstructorInfo constructorInfo)
             where TDestination : class
         {
-            var leftProperties = TypeUtils.GetPropertyDictionary<TLeft>();
-            var rightProperties = TypeUtils.GetPropertyDictionary<TRight>();
+            var leftProperties = GetPropertyDictionary<TLeft>();
+            var rightProperties = GetPropertyDictionary<TRight>();
 
+            var alreadySetProperties = new List<string>();
             var parameters = new List<object>();
 
             foreach (var parameter in constructorInfo.GetParameters())
             {
-                leftProperties.TryGetValue(parameter.Name.ToLowerInvariant(), out var leftProperty);
+                leftProperties.TryGetValue(parameter.Name, out var leftProperty);
                 var leftValue = leftProperty?.GetValue(left);
                 
-                rightProperties.TryGetValue(parameter.Name.ToLowerInvariant(), out var rightProperty);
+                rightProperties.TryGetValue(parameter.Name, out var rightProperty);
                 var rightValue = rightProperty?.GetValue(right);
 
                 var value = rightValue ?? leftValue;
                 
                 parameters.Add(value);
+                alreadySetProperties.Add(parameter.Name);
             }
 
             var constructedInstance = constructorInfo.Invoke(parameters.ToArray()) as TDestination;
 
+            var destinationProperties = GetPropertyDictionary<TDestination>();
+            
+            foreach (var pair in destinationProperties)
+            {
+                if (alreadySetProperties.Contains(pair.Key)) continue;
+                
+                leftProperties.TryGetValue(pair.Key, out var leftProperty);
+                var leftValue = leftProperty?.GetValue(left);
+                
+                rightProperties.TryGetValue(pair.Key, out var rightProperty);
+                var rightValue = rightProperty?.GetValue(right);
+
+                var value = rightValue ?? leftValue;
+                
+                pair.Value?.SetValue(constructedInstance, value);
+            }
+
             return constructedInstance;
         }
 
+        private static IDictionary<string, PropertyInfo> GetPropertyDictionary<TInstance>()
+            => TypeUtils.GetPropertyDictionary<TInstance>().Select(LowercaseKey).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+        private static KeyValuePair<string, TValue> LowercaseKey<TValue>(KeyValuePair<string, TValue> pair)
+        {
+            var lowercasedKey = Lowercase(pair.Key);
+
+            return new KeyValuePair<string, TValue>(lowercasedKey, pair.Value);
+
+            string Lowercase(string s)
+            {
+                var firstLetter = s[0];
+                var firstLetterInUppercase = char.ToLowerInvariant(firstLetter);
+                var remainingString = s.Substring(1);
+                    
+                return firstLetterInUppercase + remainingString;
+            }
+        }
+        
         private static TDestination MergeByProperty<TDestination, TLeft, TRight>(
             TLeft left, 
             TRight right)
             where TDestination : class
         {
-            var leftProperties = TypeUtils.GetPropertyDictionary<TLeft>();
-            var rightProperties = TypeUtils.GetPropertyDictionary<TRight>();
-            var destinationProperties = TypeUtils.GetPropertyDictionary<TDestination>();
+            var leftProperties = GetPropertyDictionary<TLeft>();
+            var rightProperties = GetPropertyDictionary<TRight>();
+            var destinationProperties = GetPropertyDictionary<TDestination>();
 
             var constructedInstance = Activator.CreateInstance<TDestination>();
-            
+
+            var properties = new List<string>();
+
             foreach (var parameter in destinationProperties)
             {
                 leftProperties.TryGetValue(parameter.Key.ToLowerInvariant(), out var leftProperty);
@@ -72,8 +117,25 @@ namespace Typesafe.Merge
                 
                 var setMethod = parameter.Value.GetSetMethod();
                 setMethod.Invoke(constructedInstance, new[] {value});
+                
+                properties.Add(parameter.Key);
             }
 
+            foreach (var pair in leftProperties)
+            {
+                if (properties.Contains(pair.Key)) continue;
+                
+                leftProperties.TryGetValue(pair.Key, out var leftProperty);
+                var leftValue = leftProperty?.GetValue(left);
+                
+                rightProperties.TryGetValue(pair.Key, out var rightProperty);
+                var rightValue = rightProperty?.GetValue(right);
+
+                var value = rightValue ?? leftValue;
+                
+                rightProperty?.SetValue(constructedInstance, value);
+            }
+            
             return constructedInstance;
         }
     }
