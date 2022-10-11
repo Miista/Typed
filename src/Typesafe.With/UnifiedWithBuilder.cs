9e9fd51706dc3afb4f9aee6a3dbd3853a0ba802b
Søren Guldmund <sgu@immeo.dk>
2022-10-11 12:17:26 +0200

    Reorder method layout in class

--- src/Typesafe.With/UnifiedWithBuilder.cs
+++ src/Typesafe.With/UnifiedWithBuilder.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 using Typesafe.Kernel;
 
@@ -39,85 +38,6 @@ namespace Typesafe.With
             var enrichedInstanceWithCopiedProperties = CopyProperties(instance, enrichedInstance, copyProperties);
             
             return enrichedInstanceWithCopiedProperties;
-        }
-
-        /// <summary>
-        /// Mutates <paramref name="instance"/>, setting all properties in <paramref name="newProperties"/>.
-        /// </summary>
-        /// <param name="instance">The instance to mutate.</param>
-        /// <param name="newProperties">The properties to set.</param>
-        /// <param name="valueResolver">The value resolver.</param>
-        /// <typeparam name="TInstance">The instance type.</typeparam>
-        /// <returns>A mutated instance.</returns>
-        /// <exception cref="InvalidOperationException">If the property does not exist or cannot be written to.</exception>
-        /// <exception cref="ArgumentNullException">If any of the arguments are null.</exception>
-        private static (TInstance Instance, IReadOnlyDictionary<string, object> RemainingProperties) EnrichByProperty<TInstance>(
-            TInstance instance,
-            IReadOnlyDictionary<string, object> newProperties,
-            ValueResolver<TInstance> valueResolver)
-        {
-            var existingProperties = (IDictionary<string, PropertyInfo>) TypeUtils.GetPropertyDictionary<TInstance>();
-            var remainingProperties = new Dictionary<string, object>(newProperties.ToDictionary(pair => pair.Key, pair => pair.Value));
-
-            foreach (var property in newProperties)
-            {
-                if (!existingProperties.TryGetValue(property.Key, out var existingProperty))
-                {
-                    throw new InvalidOperationException($"Cannot find property with name '{property.Key}'.");
-                }
-
-                if (!existingProperty.CanWrite)
-                {
-                    throw new InvalidOperationException($"Property '{property.Key}' cannot be written to.");
-                }
-
-                var value = valueResolver.Resolve(property.Value, existingProperty);
-                
-                existingProperty.SetValue(instance, value);
-                remainingProperties.Remove(property.Key);
-            }
-
-            return (instance, remainingProperties);
-        }
-        
-        private static T CopyProperties(T source, T destination, IEnumerable<PropertyInfo> properties)
-        {
-            foreach (var kvp in properties)
-            {
-                var value = kvp.GetValue(source);
-                kvp.SetValue(destination, value);
-            }
-
-            return destination;
-        }
-
-        private static IEnumerable<PropertyInfo> GetCopyProperties(
-            ConstructorInfo constructorInfo,
-            IReadOnlyDictionary<string, object> excludeProperties)
-        {
-            var publicProperties = (IDictionary<string, PropertyInfo>) TypeUtils.GetPropertyDictionary<T>();
-            
-            // Remove properties already set
-            foreach (var parameter in excludeProperties.Select(kvp => kvp.Key))
-            {
-                publicProperties.Remove(parameter);
-            }
-            
-            // Remove properties set via constructor
-            foreach (var parameter in GetConstructorParameterNames())
-            {
-                publicProperties.Remove(parameter);
-            }
-
-            return publicProperties.Values.Where(info => info.CanWrite);
-            
-            IEnumerable<string> GetConstructorParameterNames()
-            {
-                return constructorInfo
-                    .GetParameters()
-                    .Select(parameterInfo => parameterInfo.Name)
-                    .ToList();
-            }
         }
 
         private static (TInstance Instance, IReadOnlyDictionary<string, object> RemainingProperties) WithByConstructor<TInstance>(
@@ -167,6 +87,85 @@ namespace Typesafe.With
                 
                 return (existingPropertyByLowercaseMatch, existingPropertyKey);
             }
+        }
+
+        /// <summary>
+        /// Mutates <paramref name="instance"/>, setting all properties in <paramref name="newProperties"/>.
+        /// </summary>
+        /// <param name="instance">The instance to mutate.</param>
+        /// <param name="newProperties">The properties to set.</param>
+        /// <param name="valueResolver">The value resolver.</param>
+        /// <typeparam name="TInstance">The instance type.</typeparam>
+        /// <returns>A mutated instance.</returns>
+        /// <exception cref="InvalidOperationException">If the property does not exist or cannot be written to.</exception>
+        /// <exception cref="ArgumentNullException">If any of the arguments are null.</exception>
+        private static (TInstance Instance, IReadOnlyDictionary<string, object> RemainingProperties) EnrichByProperty<TInstance>(
+            TInstance instance,
+            IReadOnlyDictionary<string, object> newProperties,
+            ValueResolver<TInstance> valueResolver)
+        {
+            var existingProperties = (IDictionary<string, PropertyInfo>) TypeUtils.GetPropertyDictionary<TInstance>();
+            var remainingProperties = new Dictionary<string, object>(newProperties.ToDictionary(pair => pair.Key, pair => pair.Value));
+
+            foreach (var property in newProperties)
+            {
+                if (!existingProperties.TryGetValue(property.Key, out var existingProperty))
+                {
+                    throw new InvalidOperationException($"Cannot find property with name '{property.Key}'.");
+                }
+
+                if (!existingProperty.CanWrite)
+                {
+                    throw new InvalidOperationException($"Property '{property.Key}' cannot be written to.");
+                }
+
+                var value = valueResolver.Resolve(property.Value, existingProperty);
+                
+                existingProperty.SetValue(instance, value);
+                remainingProperties.Remove(property.Key);
+            }
+
+            return (instance, remainingProperties);
+        }
+
+        private static IEnumerable<PropertyInfo> GetCopyProperties(
+            ConstructorInfo constructorInfo,
+            IReadOnlyDictionary<string, object> excludeProperties)
+        {
+            var publicProperties = (IDictionary<string, PropertyInfo>) TypeUtils.GetPropertyDictionary<T>();
+            
+            // Remove properties already set
+            foreach (var parameter in excludeProperties.Select(kvp => kvp.Key))
+            {
+                publicProperties.Remove(parameter);
+            }
+            
+            // Remove properties set via constructor
+            foreach (var parameter in GetConstructorParameterNames())
+            {
+                publicProperties.Remove(parameter);
+            }
+
+            return publicProperties.Values.Where(info => info.CanWrite);
+            
+            IEnumerable<string> GetConstructorParameterNames()
+            {
+                return constructorInfo
+                    .GetParameters()
+                    .Select(parameterInfo => parameterInfo.Name)
+                    .ToList();
+            }
+        }
+
+        private static T CopyProperties(T source, T destination, IEnumerable<PropertyInfo> properties)
+        {
+            foreach (var kvp in properties)
+            {
+                var value = kvp.GetValue(source);
+                kvp.SetValue(destination, value);
+            }
+
+            return destination;
         }
     }
 }
