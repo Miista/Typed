2b218a5a024fd16bdafb0356e73d62320ba592a9
Søren Guldmund <sgu@immeo.dk>
2023-01-05 10:01:59 +0100

    WIP

---
+++ src/Typesafe.With.Tests/Mutable/MutableTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using Xunit;
+
+// ReSharper disable once CheckNamespace
+namespace Typesafe.With.Tests
+{
+    using Mutable;
+    
+    public class MutableTests
+    {
+        public class Mutable
+        {
+            internal class ClassWithSetter
+            {
+                public string String { get; set; }
+            }
+
+            [Theory, AutoData]
+            internal void Mutates_existing_instance(ClassWithSetter instance, string newValue)
+            {
+                // Arrange
+
+                // Act
+                var updatedInstance = instance.With(i => i.String, newValue);
+
+                // Assert
+                updatedInstance.String.Should().BeEquivalentTo(newValue, because: "the value has changed");
+                updatedInstance.Should().BeSameAs(instance, because: "we are mutating the existing instance");
+            }
+        }
+    }
+}
