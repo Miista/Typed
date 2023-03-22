2c15da1e8005dfd4cc81ce241b82208cbade0dc5
Søren Guldmund <sgu@immeo.dk>
2023-03-22 10:09:53 +0100

    Remove unused class TypeClonerRegistry.cs

--- src/Typesafe.Snapshots/Registry/ITypeClonerRegistry.cs
+++
@@ -1,9 +0,0 @@
-using Typesafe.Snapshots.Cloner;
-
-namespace Typesafe.Snapshots.Registry
-{
-    public interface ITypeClonerRegistry
-    {
-        bool TryGetCloner<T>(out ITypeCloner<T> cloner);
-    }
-}
