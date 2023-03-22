8a8821cf96e8e4cf07df4f6e14de62d676ae8378
Søren Guldmund <sgu@immeo.dk>
2023-03-22 10:14:41 +0100

    Move method ImplementsInterface to Typesafe.Snapshots

--- src/Typesafe.Snapshots/TypeExtensions.cs
+++ src/Typesafe.Snapshots/TypeExtensions.cs
@@ -6,6 +6,13 @@ namespace Typesafe.Snapshots
 {
     internal static class TypeExtensions
     {
+        public static bool ImplementsInterface<TInterface>(this Type type)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+
+            return type.GetInterfaces().OfType<TInterface>().Any();
+        }
+
         public static bool HasCopyConstructor(this Type type)
         {
             if (type == null) throw new ArgumentNullException(nameof(type));
